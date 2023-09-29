de5af7a5066c93f1bbb0a429eb52ffa5b3c8172d
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-09-29 08:34:18 +0000

    Deploying to gh-pages from @ MarkBellinzon/goit-js-hw-08@0e2390abfed41d5cf4283194e8cdb599575d521f 🚀

--- 01-gallery.c7a24669.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".gallery").map((function(e){return e.preview,e.original,e.description,'<div class = "gallery_item">< a class = "gallery_link" href = "$(original)" ><img class="gallery_image" src="$(preview)" data-source="$(description)" /></ ></div > '}));
-//# sourceMappingURL=01-gallery.c7a24669.js.map
--- 01-gallery.cc0e5386.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".gallery").map((({preview:e,original:i,description:l})=>'<div class = "gallery_item">< a class = "gallery_link" href = "$(original)" ><img class="gallery_image" src="$(preview)" data-source="$(description)" /></ ></div > '));
-//# sourceMappingURL=01-gallery.cc0e5386.js.map
