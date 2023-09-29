0e2390abfed41d5cf4283194e8cdb599575d521f
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-09-29 11:29:25 +0300

    dz082

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,11 +2,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const galleryImg = document.querySelector('.gallery');
 
-const markup = galleryImg.map(
-  ({ preview, original, description }) =>
-    '<div class = "gallery_item">< a class = "gallery_link" href = "$(original)" ><img class="gallery_image" src="$(preview)" data-source="$(description)" /></ ></div > '
-);
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class = "gallery__item">
+<a class = "gallery__link" href = "${original}">
+<img class = "gallery__image"
+  src = "${preview}"
+  data-source = "${original}"
+  alt = "${description}"/>
+</a></div>`
+  )
+  .join('');
 
-// console.log(galleryItems);
+galleryImg.insertAdjacentHTML('afterbegin', markup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
