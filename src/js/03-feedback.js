3cbad57fee975953ec2a7a5622d2ea3610312910
MarkBellinzon <135432432+MarkBellinzon@users.noreply.github.com>
2023-10-02 22:03:27 +0300

    dz086

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,59 @@
 import throttle from 'lodash.throttle';
 
-const FEEDBACK_KEY = 'feedback - form - state';
+const FEEDBACK_KEY = 'feedback-form-state';
+
 const formRef = document.querySelector('.feedback-form');
 
-formRef.addEventListener('input', throttle(onFormSnput, 500));
-formRef.addEventListener('submit', onFormSubmit);
+populateForm();
 
-function onFormSnput(e) {}
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(e) {
   e.preventDefault();
   const formData = new FormData(formRef);
   formData.forEach((value, name) => console.log(value, name));
   e.currentTarget.reset();
-  licalStorage.removeItem(FEEDBACK_KEY);
+  localStorage.removeItem(FEEDBACK_KEY);
 }
+
+function onFormInput(e) {
+  let inputForm = localStorage.getItem(FEEDBACK_KEY);
+  inputForm = inputForm ? JSON.parse(inputForm) : {};
+  inputForm[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(inputForm));
+}
+
+function populateForm() {
+  let inputForm = localStorage.getItem(FEEDBACK_KEY);
+  inputForm = JSON.parse(inputForm);
+}
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   textarea: document.querySelector('feedback-form'),
+// };
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+// populateTextarea();
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+
+//   evt.currentTatget.reset();
+//   localStorage.removeItem(FEEDBACK_KEY);
+// }
+
+// function onTextareaInput(evt) {
+//   const message = evt.target.value;
+//   localStorage.setItem(FEEDBACK_KEY, message);
+// }
+
+// function populateTextarea() {
+//   const saveMessage = localStorage.getItem(FEEDBACK_KEY);
+//   if (saveMessage) {
+//     refs.textarea.value = saveMessage;
+//   }
+// }
